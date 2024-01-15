28eea8180e9db6ab11688a5b87a69ab715b8492b
Harshitha M <harshithamuniraj22@gmail.com>
2024-01-15 19:57:27 +0530

    used react-modal

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from "react";
-import './App.css';
-import ToolTip from "./ToolTip";
-
-
-const App = () => {
+import React, { useState } from "react";
+import "./App.css";
+import Modal from "react-modal";
+Modal.setAppElement("#root");
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '0%',
+    transform: 'translate(-50%, -50%)',
+    width:"50%",
+  },
+};
+function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   return (
-    <div className="container">
-        
-        <ToolTip text="This is a tooltip">
-          <h2>Hover over me</h2>
-        </ToolTip>
-        <hr/>
-        <ToolTip text="This is another tooltip">
-          <p>Hover over me to see another tooltip</p>
-        </ToolTip>
+    <div className="parent">
+      <button onClick={() => setModalIsOpen(true)}>Modal open</button>
+      <Modal
+        isOpen={modalIsOpen}
+        style={customStyles}
+      >
+        <div className="modal-overlay">
+          <button onClick={()=>setModalIsOpen(false)} className="modal-close">Close</button>
+          <p className="modal-p">Hey,Hi I'm Modal</p>
+        </div>
+      </Modal>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
